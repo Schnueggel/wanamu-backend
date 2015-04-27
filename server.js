621275ad12a6a1a6a1b42467b962f2d6c0634870
Christian Steinmann <christian.steinmann77@gmail.com>
2015-04-27 23:57:06 +0200

    Added server.js (Node main file) Added some frontend files to indicate the intend where and what to create later Added package.json Added .gitignore

---
+++ server.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Christian on 4/27/2015.
+ */
+var express = require('express');
+
+var app = express();
+var test = 0;
+
+app.use(function(req, res, next){
+    console.log(test++);
+    next();
+});
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
