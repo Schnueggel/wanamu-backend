f86ce1a60a7b9666eb8198a7a75e0939b156df43
Yannick Mortier <mail@yannickmortier.de>
2015-04-28 21:04:01 +0200

    Added File Serving

--- server.js
+++ server.js
@@ -2,22 +2,47 @@
  * Created by Christian on 4/27/2015.
  */
 var express = require('express');
+var path = require('path');
 
 var app = express();
-var test = 0;
 
-app.use(function(req, res, next){
-    console.log(test++);
-    next();
-});
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
 
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
