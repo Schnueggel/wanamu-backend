716b34bf3c6c7d1d29c1bf0ed404070c2f47067f
Yannick Mortier <mail@yannickmortier.de>
2015-04-29 21:00:05 +0200

    Added Model for Toplistings

--- src/server/controller/index.js
+++ src/server/controller/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 
 var listing = require('./listing');
+var toplisting = require('./toplisting');
 
 router.use('/listing', listing);
+router.use('/toplisting', toplisting);
 
 module.exports = router;
