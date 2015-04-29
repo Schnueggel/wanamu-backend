17edfbaed2b16828c41f0c17d496c0f260e01737
Yannick Mortier <mail@yannickmortier.de>
2015-04-29 21:32:22 +0200

    Implemented getListings in Model Listing

--- src/server/model/listing.js
+++ src/server/model/listing.js
@@ -5,20 +5,31 @@ module.exports = {
     getListing: function (id) {
         return new Promise(function (fulfill, reject) {
             config.getMysqlPool().getConnection(function (err, connection) {
-                if(err) {
+                if (err) {
                     reject(err);
                     return;
                 }
                 // Use the connection
                 connection.query('SELECT * FROM dat_anzeigen WHERE anzeigen_id = ?', [id], function (err, rows) {
-                    if(err) {
+                    if (err) {
                         reject(err);
                         return;
                     }
 
                     // And done with the connection.
                     connection.release();
-                    return fulfill(rows[0]);
+
+                    var response = {};
+
+                    if (rows[0]) {
+                        response.limit = 1;
+                        response.data = rows[0];
+                    } else {
+                        response = false;
+                    }
+
+
+                    return fulfill(response);
 
                     // Don't use the connection here, it has been returned to the pool.
                 });
@@ -29,13 +40,13 @@ module.exports = {
     deleteListing: function (id) {
         return new Promise(function (fulfill, reject) {
             config.getMysqlPool().getConnection(function (err, connection) {
-                if(err) {
+                if (err) {
                     reject(err);
                     return;
                 }
                 // Use the connection
                 connection.query('DELETE FROM dat_anzeigen WHERE anzeigen_id = ?', [id], function (err, result) {
-                    if(err) {
+                    if (err) {
                         reject(err);
                         return;
                     }
@@ -43,6 +54,52 @@ module.exports = {
                     // And done with the connection.
                     connection.release();
                     return fulfill(result.affectedRows);
+
+                    // Don't use the connection here, it has been returned to the pool.
+                });
+            });
+        });
+    },
+
+    getListings: function (limit, offset) {
+        return new Promise(function (fulfill, reject) {
+            limit = parseInt(limit) || 10;
+            offset = parseInt(offset) || 0;
+
+            config.getMysqlPool().getConnection(function (err, connection) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                // Use the connection
+                var sql = 'SELECT * FROM dat_anzeigen LIMIT ' + offset + ',' + limit;
+                connection.query(sql, function (err, rows) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+
+                    connection.query('SELECT COUNT(*) AS anzahl FROM dat_anzeigen', function (err, count) {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+
+                        // And done with the connection.
+                        connection.release();
+
+                        var response = {};
+                        response.maxresult = count[0].anzahl;
+                        response.limit = limit;
+                        response.offset = offset;
+                        response.data = rows;
+
+
+                        return fulfill(response);
+
+                        // Don't use the connection here, it has been returned to the pool.
+                    });
 
                     // Don't use the connection here, it has been returned to the pool.
                 });
