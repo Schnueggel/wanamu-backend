249860018d1e8a312a56b1d5457f53d91aa81d1d
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-01 09:17:11 +0200

    Added home template Remove  some boilerplate code from home index.js

--- src/app/components/home/index.js
+++ src/app/components/home/index.js
@@ -12,42 +12,6 @@ module.exports = angular.module('home', [])
                 template: 'template.html',
                 controller: 'HomeCtrl',
                 role: 'public'
-            })
-            .state('home.index', {
-                url: '/',
-                template: require('./home.html'),
-                role: 'public'
-            })
-            .state('home.contact', {
-                url: '/contact',
-                template: require('./contact.html'),
-                role: 'public'
-            })
-            .state('home.termsofuse', {
-                url: '/termsofuse',
-                template: require('./termsofuse.html'),
-                role: 'public'
-            })
-            .state('home.about', {
-                url: '/about',
-                template: require('./about.html'),
-                role: 'public'
-            })
-            .state('home.connectionerror', {
-                url: '/connectionerror',
-                template: require('./connectionerror.html'),
-                role: 'public'
-            })
-            .state('home.accessdenied', {
-                url: '/accessdenied',
-                template: require('./accessdenied.html'),
-                role: 'public'
-            })
-            .state('home.servererror', {
-                url: '/servererror',
-                template: require('./servererror.html'),
-                role: 'public'
             });
-        // END
     }])
     .controller('HomeCtrl', ['$scope', '$state', '$window', '$filter',  function ($scope, $state, $window, $filter) {}]);
