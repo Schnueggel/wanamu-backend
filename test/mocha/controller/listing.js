4539aa83f4dedd7527bba452c1aff8ba0fd7447d
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-09 13:08:07 +0200

    Mocha Test for ListingController

--- test/mocha/controller/listing.js
+++ test/mocha/controller/listing.js
@@ -3,12 +3,15 @@
  * Created by Christian on 5/2/2015.
  */
 var assert = require('assert'),
-    listing = require('../../../src/server/server/controller/listing');
+    ListingController = require('../../../dist/server/server/controller/Listing'),
+    listingModel = require('../../../dist/server/server/model/Listing');
 
-
-describe('Get Listing Controller Object', function () {
+describe('Get Listing Controller Class', function () {
     it('Should have a function named get and list', function () {
-        assert.equal(typeof listing.get, 'function');
-        assert.equal(typeof listing.list, 'function');
+        assert.equal(typeof ListingController, 'function');
+        var lc = new ListingController(listingModel);
+        assert.equal(typeof lc, 'object');
+        assert.equal(typeof lc.list, 'function');
+        assert.equal(typeof lc.get, 'function');
     });
 });
