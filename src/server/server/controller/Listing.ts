b2874e989d5899ad8c53438614bd6a5ceed38b7d
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-10 23:09:14 +0200

    Added create action to listing controller

--- src/server/server/controller/Listing.ts
+++ src/server/server/controller/Listing.ts
@@ -1,14 +1,86 @@
 'use strict';
 
+var User = require('../model/user.js'),
+    Category = require('../model/category.js');
+
 /**
  * Listing Controller
  */
 class Listing {
     listingModel: Model;
+
+    /**
+     * Create Listing
+     * @param listingModel
+     */
     constructor (listingModel: Model) {
         this.listingModel = listingModel;
     }
-    get(request, response) {
+
+    /**
+     * Update a listing
+     * @param request
+     * @param response
+     */
+    update (request, response) {
+
+    }
+
+    /**
+     * Create a listing
+     * @param request
+     * @param response
+     */
+    create (request, response) {
+
+        var input = request.body || {};
+        var user = request.session.user;
+
+        input.user = user.id;
+
+        this.listingModel
+            .create(input, {
+                isNewRecord: true,
+                include: [
+                    { model: Category, nested: true}
+                ]
+            })
+            .then(function(listing){
+                listing
+                    .reload()
+                    .then(function(result){
+                        response.send(result.get({
+                            plain: true
+                        }));
+                    });
+            })
+            .catch(function(err) {
+                console.error(err);
+                response.send(500);
+            });
+    }
+
+    /**
+     * Delete a listing
+     * @param request
+     * @param response
+     */
+    delete (request, response) {
+
+    }
+
+    /**
+     * Get a listing
+     * @param request
+     * @param response
+     */
+    get (request, response) {
+
+        if (request.params.id === undefined) {
+            console.error('Get:Listing missing id');
+            response.sendStatus(403);
+            return;
+        }
         // ==========================================================================
         // Default result object
         // ==========================================================================
@@ -33,14 +105,18 @@ class Listing {
             }
             result.data.push(res.toJSON());
             response.send(result);
-        }).catch(function (err) {
+        }).catch((err) => {
             response.sendStatus(500);
             console.log(err);
         });
     }
 
+    /**
+     * Get multiple listings
+     * @param request
+     * @param response
+     */
     list(request, response) {
-        console.log(request.params);
         var result = {
             limit: request.param('limit', 1000),
             offset: request.param('offset', 0),
@@ -52,14 +128,16 @@ class Listing {
             offset: result.offset
         }, {
             raw: true
-        }).then(function (res) {
+        }).then((res) => {
             result.data = res.rows;
             result.total = res.total;
             response.send(result);
-        }).catch(function (err) {
+        }).catch((err) => {
             response.send(err);
             console.error(err);
         });
     }
 }
+
+
 export = Listing;
