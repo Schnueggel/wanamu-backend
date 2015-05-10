8bf87c1a62749cab149b185430ff16412875c753
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-10 23:10:39 +0200

    Created development database setupscript

---
+++ src/server/server/setup/development/database.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Christian on 5/10/2015.
+ */
+'use strict';
+
+var User = require('../../model/user.js'),
+    UserGroup = require('../../model/user-group.js'),
+    Category = require('../../model/category.js'),
+    co = require('co');
+
+/**
+ * Starts the database setup
+ * @returns {Promise}
+ */
+function start() {
+    return co(setup);
+}
+
+/**
+ * Setup complete database
+ */
+function* setup(){
+    yield createCategories();
+    yield createUserGroups();
+    yield createUsers();
+    console.log('user created');
+}
+
+
+function* createUsers() {
+    yield User.create({
+        email: 'test@email.de',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        password: 'abcdef',
+        userGroup: 1
+    }, { isNewRecord: true });
+    console.log('done');
+}
+
+function* createCategories() {
+    yield Category.bulkCreate([
+        {id: 1, name: 'category1' },
+        {id: 2, name: 'category2' },
+        {id: 3, name: 'category3', parent: 1}
+    ]);
+}
+
+function* createUserGroups() {
+    yield UserGroup.bulkCreate([
+        {id: 1, name: 'private', flag: 'P' },
+        {id: 2, name: 'business', flag: 'G'},
+        {id: 3, name: 'admin', flag: 'A'}
+    ]);
+}
+
+module.exports = start();
