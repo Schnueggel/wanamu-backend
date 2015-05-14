2afe71c01565290383bf637f5b09a3ecc242e92a
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-14 11:58:10 +0200

    Added comments

--- src/server/server/controller/listing.js
+++ src/server/server/controller/listing.js
@@ -21,6 +21,9 @@ function* updateListing(req, res) {
         input = req.body || {},
         listing = null;
 
+    // ==========================================================================
+    // Try to find the listing
+    // ==========================================================================
     try {
         listing = yield ListingModel.findOne(req.params.id);
         if (!listing ) {
@@ -31,13 +34,18 @@ function* updateListing(req, res) {
         res.status(404).send('Listing could not be found');
         return;
     }
-
+    // ==========================================================================
+    // Check Permissions
+    // ==========================================================================
     if (user.UserGroup.name !== 'admin' && listing.get('userId') !== user.get('id')) {
         console.error('Listing does not belong to user');
         res.status(403).send('Listing does not belong to user');
         return;
     }
 
+    // ==========================================================================
+    // Update Listing
+    // ==========================================================================
     try {
         yield listing.updateAttributes(input, {
             // Fields Allowed to updated
@@ -50,13 +58,18 @@ function* updateListing(req, res) {
         res.status(422).send(err);
         return;
     }
-
+    // ==========================================================================
+    // Reload data from database
+    // ==========================================================================
     yield listing.reload({
         include: [
             {model: Category, nested: true}
         ]
     });
 
+    // ==========================================================================
+    // Send to client
+    // ==========================================================================
     var data = listing.get({
         plain: true
     });
@@ -67,11 +80,19 @@ function* updateListing(req, res) {
 
 }
 
+/**
+ * Destroys a listing
+ * @param req
+ * @param res
+ */
 function* destroyListing(req, res) {
 
     let user = req.user,
         listing = null;
 
+    // ==========================================================================
+    // Try to find Listing
+    // ==========================================================================
     try {
         listing = yield Listing.find(req.params.id);
     } catch (err) {
@@ -79,12 +100,18 @@ function* destroyListing(req, res) {
         res.status(404).send('Listing could not be found');
         return;
     }
+    // ==========================================================================
+    // Check permissions
+    // ==========================================================================
     if (user.UserGroup.name !== 'admin' && listing.get('userId') != user.get('id')) {
         console.error('Listing does not belong to user');
         res.status(401).send('Listing does not belong to user');
         return;
     }
 
+    // ==========================================================================
+    // Destroy listing
+    // ==========================================================================
     try {
         yield listing.destroy();
     } catch (err) {
@@ -92,16 +119,24 @@ function* destroyListing(req, res) {
         res.status(500).send('Error deleting the listing');
         return;
     }
-
     res.status(200).send({});
 
 }
 
+/**
+ * Creates a listing
+ * Expects request.body to be set
+ * @param req
+ * @param res
+ */
 function* createListing(req, res) {
     let input = req.body || {},
         user = req.user,
         listing = null;
 
+    // ==========================================================================
+    // Try to create a new listing
+    // ==========================================================================
     try {
         input.userId = user.get('id');
         listing = ListingModel.build(input, {
@@ -111,16 +146,23 @@ function* createListing(req, res) {
         yield listing.save();
     } catch (err) {
         console.error(err);
+        // TODO distinct between validation errors and other errors
         res.status(422).send(err);
         return;
     }
 
+    // ==========================================================================
+    // Reload data from database
+    // ==========================================================================
     yield listing.reload({
         include: [
             {model: Category, nested: true}
         ]
     });
 
+    // ==========================================================================
+    // Send to client
+    // ==========================================================================
     var data = listing.get({
         plain: true
     });
@@ -130,6 +172,12 @@ function* createListing(req, res) {
     });
 }
 
+/**
+ * Get a single Listing
+ * Needs request.params.id field
+ * @param req
+ * @param res
+ */
 function* getListing(req, res) {
     // ==========================================================================
     // Default result object
@@ -167,19 +215,29 @@ function* getListing(req, res) {
     res.send(result);
 }
 /**
- * Sends a list of json
+ * Get a list of Listings
+ * Accepts request params
+ * {
+ *  query {
+ *      limit: [int],
+ *      offset: [int]
+*   }
+ * }
  * @param req
  * @param res
  */
 function* listListing(req, res) {
     let result = {
-            limit: req.params.limit ? req.params.limit : 1000,
-            offset: req.params.offset !== 0 ? req.params.offset : 0,
+            limit: req.query.limit ? req.query.limit : 1000,
+            offset: req.query.offset !== 0 ? req.query.offset : 0,
             data: [],
             total: 0
         },
         listings = null;
 
+    // ==========================================================================
+    // Try to find listings
+    // ==========================================================================
     try {
         listings = yield ListingModel.findAndCountAll({
             limit: result.limit,
@@ -192,6 +250,9 @@ function* listListing(req, res) {
         res.status(500).send(err);
     }
 
+    // ==========================================================================
+    // Send data
+    // ==========================================================================
     result.data = listings.rows;
     result.total = listings.total;
     res.send(result);
