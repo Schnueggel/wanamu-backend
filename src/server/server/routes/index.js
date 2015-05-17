1f2197e62fc18dfe83eba27287fa7bd72d5e7920
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-17 23:43:01 +0200

    Implemented Auth check for routes

--- src/server/server/routes/index.js
+++ src/server/server/routes/index.js
@@ -1,8 +1,23 @@
 var co = require('co'),
-    route = require('koa-route');
+    route = require('koa-route'),
+    TodoController = require('../controller/todo.js'),
+    AuthController = require('../controller/auth.js');
 
 module.exports = function(app){
-    app.use(route.get('/', function*(){
-        this.body  = 'Hello World';
-    }));
+    app.use(route.post('/todo/:userid/:todolist', auth(TodoController.create)));
+    app.use(route.post('/auth/login', AuthController.login))
 };
+
+
+function auth(fn){
+    return function* () {
+        if (!this.isAuthenticated()) {
+            this.status = 403;
+            this.body = { success: false }
+        } else {
+            console.log(fn);
+            console.log(arguments);
+            yield* fn.apply(this, arguments);
+        }
+    };
+}
