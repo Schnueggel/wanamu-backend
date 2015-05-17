c9e1654725faf93f47bdab01b9bfb30efb0ef398
Christian Steinmann <christian.steinmann77@gmail.com>
2015-05-17 14:33:08 +0200

    Created user model

--- src/server/server/model/user.js
+++ src/server/server/model/user.js
@@ -2,9 +2,40 @@
  * Created by Christian on 5/15/2015.
  */
 
-var mongo = require('../config/mongo.js')
+var mongo = require('../config/mongo.js'),
+    bcrypt = require('../config/bcrypt.js'),
+    co = require('co'),
+    Util = require('../util/util.js'),
     wrap = require('co-monk');
 
-var users = wrap(db.get('users'));
+var monkcoll =  mongo.get('users');
+var users = wrap(monkcoll);
 
-module.exports = users;
+users.index('email', { unique: true });
+
+var User = {};
+
+User.prototype = users;
+
+
+User.create = function* (input) {
+    var data = input || {};
+
+
+    if (!data.password || data.length < 8) {
+        throw Util.ERRORS.INVALID_USER_PASSWORD;
+    }
+
+    var password = yield bcrypt.hashAndSalt(data.password);
+    console.log(password);
+    if (!password && password.length !== 60) {
+        throw Util.ERRORS.USER_PASSWORD_NOT_CREATED;
+    }
+    data.password = password;
+
+    var user = yield users.insert(data);
+    console.log(user);
+    return data;
+};
+
+module.exports = User;
