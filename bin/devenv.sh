4ba24c3c23da4ed6fd33347a4ee5184a53eb786a
Christian Steinmann <christian.steinmann77@gmail.com>
2015-07-22 23:11:51 +0200

    Default env setup for development

--- bin/devenv.sh
+++ bin/devenv.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-EXPORT WANAMU_DB_PASSWORD=wanamu
-EXPORT WANAMU_DB_USER=wanamu
-EXPORT WANAMU_DATABASE=wanamu
-EXPORT WANAMU_ENV=development
+
+export WU_DB_PASSWORD=wanamu
+export WU_DB_USER=wanamu
+export WU_DB_NAMEE=wanamu
+export WU_ENV=development
+export WU_DB_HOST=localhost:5432
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgress
+export WU_HTTP_AUTH=""
+export WU_HTTP_USER=""
+export WU_HTTP_PASSWORD=""
+export WU_ENV=development
+
