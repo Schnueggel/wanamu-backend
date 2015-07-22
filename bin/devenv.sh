5604d9aa0ad4a355fd69df38e7f2dc7569503183
Christian Steinmann <christian.steinmann77@gmail.com>
2015-07-22 23:49:41 +0200

    Fixed env names

--- bin/devenv.sh
+++ bin/devenv.sh
@@ -2,7 +2,7 @@
 
 export WU_DB_PASSWORD=wanamu
 export WU_DB_USER=wanamu
-export WU_DB_NAMEE=wanamu
+export WU_DB_NAME=wanamu
 export WU_ENV=development
 export WU_DB_HOST=localhost:5432
 export POSTGRES_USER=postgres
@@ -10,5 +10,4 @@ export POSTGRES_PASSWORD=postgress
 export WU_HTTP_AUTH=""
 export WU_HTTP_USER=""
 export WU_HTTP_PASSWORD=""
-export WU_ENV=development
 
