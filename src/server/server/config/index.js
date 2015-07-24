aa96b9e560b8b980877e12ca000dc986f78e8b83
Christian Steinmann <christian.steinmann77@gmail.com>
2015-07-25 00:46:40 +0200

    Config to es6

--- src/server/server/config/index.js
+++ src/server/server/config/index.js
@@ -4,7 +4,6 @@ let nconf = require('nconf');
 let fs = require('fs'),
     _ = require('lodash');
 
-
 // ==========================================================================
 // ENVIRONMENT VARS
 // ==========================================================================
@@ -13,101 +12,145 @@ const TEST = 'test';
 const PRODUCTION = 'production';
 const STAGING = 'staging';
 
-// =============================================================================================
-// Constant keys
-// =============================================================================================
-nconf.statics = {
-    SEQUELIZE: 'sequelize',
-    WU_ENV: 'WU_ENV',
-    WU_HTTP_AUTH: 'WU_HTTP_AUTH',
-    WU_HTTP_USER: 'WU_HTTP_USER',
-    WU_HTTP_PASSWORD: 'WU_HTTP_PASSWORD',
-    WU_DB_HOST: 'WU_DB_HOST',
-    WU_DB_NAME: 'WU_DB_NAME',
-    WU_DB_USER: 'WU_DB_USER',
-    WU_DB_PASSWORD: 'WU_DB_PASSWORD'
-};
-
-// =============================================================================================
-// Not sure if this should be loaded first or last. At moment it will be loaded first and
-// And overwritten ny the config files
-// =============================================================================================
-nconf.argv();
-nconf.env({
-    separator: '__',
-    match: /WANAMU_.+/
-});
-
-// =============================================================================================
-// Check for correct environment variable
-// =============================================================================================
-let env = nconf.get(nconf.statics.WU_ENV);
-
-console.log(`Generate config for: ${env}`);
-
-// ==========================================================================
-// Convenient methods for checking the environment
-// ==========================================================================
-nconf.isDevelopment = function() {return env === DEVELOPMENT;};
-nconf.isProduction = function() {return env === PRODUCTION;};
-nconf.isStaging = function() {return env === STAGING;};
-nconf.isTest = function() {return env === TEST;};
-
-let environments = [DEVELOPMENT, TEST, PRODUCTION, STAGING];
-
-if (!_.contains(environments, env)) {
-    throw new Error('Invalid server environment found:' + env);
-}
-
-
-// =============================================================================================
-// The default config file
-// =============================================================================================
-nconf.file({file: __dirname + '/json/default.json'});
-
-// =============================================================================================
-// Test inherits from development
-// =============================================================================================
-if (nconf.isTest()) {
-    nconf.file('development', {file: __dirname + '/json/development.json'});
-}
-// =============================================================================================
-// Load the environment specific config
-// =============================================================================================
-nconf.file('envconfig', {file: __dirname + '/json/' + env.toLowerCase() + '.json'});
-
-// =============================================================================================
-// If a local.json exist it will overwrite any config
-// =============================================================================================
-if (fs.existsSync( __dirname + '/json/local.json')) {
-    nconf.file('local', {file: __dirname + '/json/local.json'});
-}
-
 /**
- * Returns the confirmation url
- * @param hash
- * @returns {*}
+ * Config File
  */
-nconf.getConfirmationUrl = function(hash){
-    return nconf.get('webhost') + nconf.get('url').confirmation.replace('${hash}', hash);
-};
-/**
- * Returns the default mail for testing
- */
-nconf.getTestMail1 = function() {
-    return nconf.get('testmail1');
-};
+class Config {
 
-/**
- * Returns the url of the home page for the webfrontend
- * @returns {*}
- */
-nconf.getWebhomeUrl = function(){
-    return nconf.get('webhost') + nconf.get('webhome');
-};
+    constructor() {
+        this.nconf = nconf;
+        this.environments = [DEVELOPMENT, TEST, PRODUCTION, STAGING];
+        // =============================================================================================
+        // Not sure if this should be loaded first or last. At moment it will be loaded first and
+        // And overwritten ny the config files
+        // =============================================================================================
+        nconf.argv();
+        nconf.env({
+            separator: '__',
+            match: /WANAMU_.+/
+        });
+
+        // =============================================================================================
+        // Check for correct environment variable
+        // =============================================================================================
+        this.env = nconf.get(this.statics.WU_ENV);
+
+        console.log(`Generate config for: ${this.env}`);
+
+        if (!_.contains(this.environments, this.env)) {
+            throw new Error(`Invalid server environment found: ${this.env}`);
+        }
+
+        // =============================================================================================
+        // The default config file
+        // =============================================================================================
+        nconf.file({file: __dirname + '/json/default.json'});
+
+        // =============================================================================================
+        // Test inherits from development
+        // =============================================================================================
+        if (this.isTest()) {
+            let path  = `${__dirname}/json/${DEVELOPMENT}.json`;
+            nconf.file('development', {file: path });
+        }
+        // =============================================================================================
+        // Load the environment specific config
+        // =============================================================================================
+        let envConfigPath = `${__dirname}/json/{env.toLowerCase()}.json`;
+        nconf.file('envconfig', { file: envConfigPath });
+
+        // =============================================================================================
+        // If a local.json exist it will overwrite any config
+        // =============================================================================================
+        if (fs.existsSync(__dirname + '/json/local.json')) {
+            nconf.file('local', {file: __dirname + '/json/local.json'});
+        }
+    }
+
+    get statics() {
+        return {
+            SEQUELIZE: 'sequelize',
+            WU_ENV: 'WU_ENV',
+            WU_HTTP_AUTH: 'WU_HTTP_AUTH',
+            WU_HTTP_USER: 'WU_HTTP_USER',
+            WU_HTTP_PASSWORD: 'WU_HTTP_PASSWORD',
+            WU_DB_HOST: 'WU_DB_HOST',
+            WU_DB_NAME: 'WU_DB_NAME',
+            WU_DB_USER: 'WU_DB_USER',
+            WU_DB_PASSWORD: 'WU_DB_PASSWORD'
+        };
+    }
+
+    isDevelopment() {
+        return this.env === DEVELOPMENT;
+    }
+
+    isProduction() {
+        return this.env === PRODUCTION;
+    }
+
+    isStaging() {
+        return this.env === STAGING;
+    }
+
+    isTest() {
+        return this.env === TEST;
+    }
+
+    /**
+     * Returns the confirmation url
+     * @param hash
+     * @returns {*}
+     */
+    getConfirmationUrl(hash) {
+        return nconf.get('webhost') + nconf.get('url').confirmation.replace('${hash}', hash);
+    }
+
+    /**
+     * Returns the default mail for testing
+     */
+    getTestMail1() {
+        return nconf.get('testmail1');
+    }
+
+    /**
+     * Returns the url of the home page for the webfrontend
+     * @returns {*}
+     */
+    getWebhomeUrl() {
+        return nconf.get('webhost') + nconf.get('webhome');
+    }
+
+    /**
+     * Return nconf object
+     * @returns {*|exports|module.exports|nconf}
+     */
+    getNconf() {
+        return this.nconf;
+    }
+
+    /**
+     * Wrapper for nconf.get
+     * @param key
+     * @returns {*}
+     */
+    get(key) {
+        return this.nconf.get(key);
+    }
+
+    /**
+     * Wrapper for nconf.set
+     * @param key
+     * @param value
+     */
+    set(key, value) {
+        this.nconf.set(key, value);
+    }
+
+}
 
 // =============================================================================================
 // Export
 // =============================================================================================
-module.exports = nconf;
+module.exports = new Config();
 
--- src/server/server/config/sequelize.js
+++ src/server/server/config/sequelize.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let nconf = require('../config'),
+let config = require('../config'),
     Sequelize = require('sequelize');
 
-let sequelize = nconf.get(nconf.statics.SEQUELIZE);
+let sequelize = config.get(config.statics.SEQUELIZE);
 
-sequelize.host = nconf.get(nconf.statics.WU_DB_HOST);
+sequelize.host = config.get(config.statics.WU_DB_HOST);
 
-module.exports = new Sequelize(nconf.get(nconf.statics.WU_DB_NAME), nconf.get(nconf.statics.WU_DB_USER), nconf.get(nconf.statics.WU_DB_PASSWORD), sequelize);
+module.exports = new Sequelize(config.get(config.statics.WU_DB_NAME), config.get(config.statics.WU_DB_USER), config.get(config.statics.WU_DB_PASSWORD), sequelize);
