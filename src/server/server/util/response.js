fcdcdf72f96e0889730c351b4cbebd1a8f3057f8
Christian Steinmann <christian.steinmann77@gmail.com>
2015-08-01 02:11:03 +0200

    Added Reponse Class

---
+++ src/server/server/util/response.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export class Response {
+     constructor () {
+         this.data = [];
+         this.success = false;
+         this.message = null;
+         this.error = null;
+     }
+}
